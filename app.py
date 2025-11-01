770d906652b66d392d4b5a915f5787ab79a36b26
aryanraj7791 <aryanraj5371@gmail.com>
2025-11-02 01:17:16 +0530

    latest commit

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, request, jsonify
 import joblib
+import requests, os
 import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app) # Allows requests from react frontend
+
+MODEL_URL = "https://drive.google.com/uc?export=download&id=10WQt1AEH3yvyJbmq5bqfwCnbtxcTL4u_&confirm=t"
+PIPELINE_URL = "https://drive.google.com/uc?export=download&id=1ia4MSIjEPx0D4S5eKCvO1ZnIn8WogHubk"
+
+def download_file(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, "wb") as f:
+        f.write(response.content)
+
+# Download model and pipline if not exist
+if not os.path.exists("model.pkl"):
+    download_file(MODEL_URL, "model.pkl")
+
+if not os.path.exists("pipeline.pkl"):
+    download_file(PIPELINE_URL, "pipeline.pkl")
 
 # Load the model and pipeline
 model = joblib.load("./model.pkl")
